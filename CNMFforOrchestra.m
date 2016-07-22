8915edb4b5ccb89263333091aefd1f8e207d41e0
kimerein <kimerein@gmail.com>
2016-07-22 15:45:20 -0400

    Added cvx_setup and other modifications to run on Orchestra

--- CNMFforOrchestra.m
+++ CNMFforOrchestra.m
@@ -1,8 +1,15 @@
 clear;
-%% load file
 
+curd=pwd;
+
+cd('cvx');
+cvx_setup;
+
+cd(curd);
+addpath(genpath('ca_source_extraction'));
 addpath(genpath('utilities'));
-             
+
+% load file          
 nam = 'combined1.tif';          % insert path to tiff stack here
 sframe=1;						% user input: first frame to read (optional, default 1)
 num2read=100000;					% user input: how many frames to read   (optional, default until the end)
@@ -14,17 +21,13 @@ if ~isa(Y,'double');    Y = double(Y);  end         % convert to double
 [d1,d2,T] = size(Y);                                % dimensions of dataset
 d = d1*d2;                                          % total number of pixels
 
-%% Set parameters
+% Set parameters
 
 K = 50;                                           % number of components to be found
 tau = 5;                                          % std of gaussian kernel (size of neuron) 
 p = 1;                                            % order of autoregressive system (p = 0 no dynamics, p=1 just decay, p = 2, both rise and decay)
 merge_thr = 0.75;                                  % merging threshold
-saveDir='outputOfCNMF';
 
-if ~exists(saveDir)
-    status=mkdir(saveDir);
-end
 
 options = CNMFSetParms(...                      
     'd1',d1,'d2',d2,...                         % dimensions of datasets
@@ -40,32 +43,60 @@ options = CNMFSetParms(...
     'merge_thr',merge_thr,...                   % merging threshold
     'gSig',tau...
     );
-%% Data pre-processing
+% Data pre-processing
 
 [P,Y] = preprocess_data(Y,p);
 
-%% fast initialization of spatial components using greedyROI and HALS
+Cn =  reshape(P.sn,d1,d2);
+
+% fast initialization of spatial components using greedyROI and HALS
 
 [Ain,Cin,bin,fin,center] = initialize_components(Y,K,tau,options,P);  % initialize
 
-%% update spatial components
+% update spatial components
 Yr = reshape(Y,d,T);
 clear Y;
 [A,b,Cin] = update_spatial_components(Yr,Cin,fin,Ain,P,options);
 
-%% update temporal components
+% update temporal components
 P.p = 0;    % set AR temporarily to zero for speed
 [C,f,P,S] = update_temporal_components(Yr,A,b,Cin,fin,P,options);
 
-%% merge found components
+% merge found components
 [Am,Cm,K_m,merged_ROIs,P,Sm] = merge_components(Yr,A,b,C,f,P,S,options);
 
-%% repeat
+% repeat
 P.p = p;    % restore AR value
 [A2,b2,Cm] = update_spatial_components(Yr,Cm,f,Am,P,options);
 [C2,f2,P,S2] = update_temporal_components(Yr,A2,b2,Cm,f,P,options);
 
-%% output
-[A_or,C_or,S_or,P] = order_ROIs(A2,C2,S2,P); % order components
-K_m = size(C_or,1);
-[C_df,~] = extract_DF_F(Yr,[A_or,b2],[C_or;f2],K_m+1); % extract DF/F values (optional)
+% output
+fprintf(1,'%6s\n','Yr');
+for i=1:size(Yr,1)
+    fprintf(1,'% 10.1f',Yr(i,:));
+    fprintf(1,'\n');
+end
+fprintf(1,'\n');
+
+fprintf(1,'%6s\n','b2');
+for i=1:size(b2,1)
+    fprintf(1,'% 10.4f',b2(i,:));
+    fprintf(1,'\n');
+end
+fprintf(1,'\n');
+
+fprintf(1,'%6s\n','f2');
+for i=1:size(f2,1)
+    fprintf(1,'% 6.6f',f2(i,:));
+    fprintf(1,'\n');
+end
+fprintf(1,'\n');
+
+fprintf(1,'%6s\n','Cn');
+for i=1:size(Cn,1)
+    fprintf(1,'% 10.4f',Cn(i,:));
+    fprintf(1,'\n');
+end
+fprintf(1,'\n');
+
+exit
